474368cf4b66fd7ffa3aab325ce2424a24248cd5
9juanjuan <garcia.juan1994@gmail.com>
2026-01-06 17:42:02 -0600

    logging to production mode

--- src/logger.js
+++ src/logger.js
@@ -71,14 +71,12 @@ const logger = winston.createLogger({
   ]
 });
 
-// If we're not in production, log to the console as well
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
-  }));
-}
+// Always log to console for visibility
+logger.add(new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  )
+}));
 
 module.exports = logger;
